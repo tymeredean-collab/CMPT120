44e8d06263fa24949648f72f8a80a2c215cb134a
Tymere Dean <tymeredean@gmail.com>
2026-02-02 18:56:01 -0500

    Fix indentation and print statements in main function

--- Assignment 2/Assignment2.py
+++ Assignment 2/Assignment2.py
@@ -1,15 +1,12 @@
 def main():
   Number = 10
   if Number > 12:
-print(Number, "is greater than 12")
-elif Number > 11:
-print(Number, "is greater than 11")
-elif Number == 10:
-print(Number, "is equal to 10")
-else:
-print(Number, "is less than 10")
-
-
-
+    print(Number, "is greater than 12")
+  elif Number > 11:
+    print(Number, "is greater than 11")
+  elif Number == 10:
+    print(Number, "is equal to 10")
+  else:
+    print(Number, "is less than 10")
 
 main()
