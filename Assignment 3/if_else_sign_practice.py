ac65ab26ebe6ea7afcba1a8f4919dc744409f970
Tymere Dean <tymeredean@gmail.com>
2026-02-10 14:15:37 -0500

    Add score grading program using if-else statements

---
+++ Assignment 3/if_else_sign_practice.py
@@ -0,0 +1,14 @@
+def main():
+  Score = int(input("Write your test score: "))
+  if Score >= 90:
+    print("A")
+  elif Score >= 80:
+    print("B")
+  elif Score >= 70:
+    print("C")
+  elif Score >= 60:
+    print("D")
+  elif Score <60:
+    print("F")
+
+main()
