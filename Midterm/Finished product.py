7fae0e81cbc50303b463048665281c12e3941254
Tymere Dean <tymeredean@gmail.com>
2026-02-25 16:40:27 -0500

    Enhance code readability with comments
    Added comments for clarity and improved code readability.

--- Midterm/Finished product.py
+++ Midterm/Finished product.py
@@ -2,15 +2,16 @@
 def main():
   player_name = input("What is your name: ")
   print("Hello", player_name, "Welcome to the Athlete Builder Simulator")
+ ##Our starting attributes for the player
   strength = 60
   speed = 75
   shooting = 65
   stamina = 70
   week = 1
   
-  overall = (strength + speed + shooting + stamina)//4
+  overall = (strength + speed + shooting + stamina)//4 
 
-  while overall < 90 and stamina > 0:
+  while overall < 90 and stamina > 0:   ## while loop conditioning 
     print("Week: ", week)
     print("Strength: ", strength)
     print("Speed: ", speed)
@@ -20,7 +21,9 @@ def main():
     print("2. Sprint (increase speed)")
     print("3. Shoot (increase shooting)")
     print("4. Recover (increase stamina)")
-    user_choice = int(input("Pick a number: "))
+    
+    user_choice = int(input("Pick a number: ")) ##This checks the user's input and applies their choice to their player
+    
     if user_choice == 1:
       strength += 3
       stamina -= 2
@@ -35,8 +38,9 @@ def main():
     else:
       print("invalid choice")
     week = week + 1
-    overall = (strength + speed + shooting + stamina)//4
+    overall = (strength + speed + shooting + stamina)//4 ## recalculates the overall based off the user's choice
     print("Overall: " , overall)
+    
     if overall >= 90:
       print("Congratualations, you are an elite athlete")
     elif stamina <= 0:
