b5c5cf7773a6f848e3a4048954fa215f9dbe15f5
Tymere Dean <tymeredean@gmail.com>
2026-03-26 16:56:07 -0400

    Add debug.py with various utility functions
    Implement functions for printing, addition, and comparison.

---
+++ Assignment 5/debug.py
@@ -0,0 +1,38 @@
+def printHello():
+    print("Hello")
+    
+def printName(x):
+    print(x)
+    
+def addition(x, y):
+  return(x + y)
+
+def smaller(i, j):
+  if i < j:
+    return(i)
+  elif j < i:
+    return(j)
+  else:
+    return(0)
+  
+
+def main():
+    
+    def printHello()
+    printName("Tymere")
+  
+    
+    var1= 10
+    var2= 20
+    print(addition(var1, var2))
+    
+    num1 = int(input("Enter number 1"))
+    num2 = int(input("Enter number 2"))
+    print(smaller(num1, num2))
+
+
+
+
+
+
+main()
