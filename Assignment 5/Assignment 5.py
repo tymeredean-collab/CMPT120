bc1ab85dda087ba6954759bc023f6322e18cbba0
Tymere Dean <tymeredean@gmail.com>
2026-03-26 18:00:13 -0400

    Add bust function to evaluate random numbers
    Implemented a bust function to evaluate the sum of three random numbers and determine the result based on specific conditions.

--- Assignment 5/Assignment 5.py
+++ Assignment 5/Assignment 5.py
@@ -1,1 +1,29 @@
+import random
+def bust(num1, num2, num3):
+  sum = num1 + num2 + num3
+  if sum <= 21:
+    return(sum)
+  elif num1 == 11 or num2 == 11 or num3 == 11:
+    return sum - 10
+  else:
+        return(0)
 
+
+
+
+
+
+def main():
+  val1 = random.randint(1, 11)
+  val2 = random.randint(1, 11)
+  val3 = random.randint(1, 11)
+ 
+  print("Values:", val1, val2, val3)
+  print("Result:", bust(val1, val2, val3))
+
+
+
+main()
+
+
+# I learned how to use random.randint() to generate numbers between 1 and 11, and different conditions, and return the correct value.
