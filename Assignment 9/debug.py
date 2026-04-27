6a1c7879b06379e7717c72ce566f50cc730309e3
Tymere Dean <tymeredean@gmail.com>
2026-04-26 23:11:11 -0400

    Implement name introduction feature in debug.py
    Added functionality to read names from a text file and print introductions.

--- Assignment 9/debug.py
+++ Assignment 9/debug.py
@@ -1,1 +1,23 @@
+#take the names from the text file
+#Write a short introduction for each person
+#"Hi my name is (name) and I (make up a fun fact i dont care, the more insane the better)
 
+#add your name to the text file using code :)
+
+def main():
+  file = "hw.txt"
+  f = open(file, "a")
+  f.write("Ty")
+  f.close()
+  
+  f = open(file, "r")
+  names = f.readlines()
+  f.close()
+
+  for name in names:
+        name = name.strip()
+        print(f"Hi my name is {name} and I once fought a bear over a parking spot.")
+
+
+main()
+  
